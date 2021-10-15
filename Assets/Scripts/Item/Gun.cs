fa247c334dbda03483053039d7df31f6d93af716
DenisPirog <denispirog878@gmail.com>
2021-10-15 20:25:48 +0300

    Test Audio Update

--- Assets/Scripts/Item/Gun.cs
+++ Assets/Scripts/Item/Gun.cs
@@ -6,15 +6,17 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private float damage;
+    [SerializeField] private AudioClip shootSound;
+    [SerializeField] private AudioSource audioSource;
 
     public void Shoot()
     {
+        audioSource.PlayOneShot(shootSound);
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
         ray.origin = cam.transform.position; 
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(damage);
-            Debug.Log(hit.collider.name);
         }
     }
 }
