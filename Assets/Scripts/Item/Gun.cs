417d9c97f39360b7c83703e8495c79f79af2c12c
STYFireNinja <philipp.makhroff@gmail.com>
2021-10-15 21:22:15 +0300

    Gun Update
    Added: Ammo, Fire Rate, Reload and AWP

--- Assets/Scripts/Item/Gun.cs
+++ Assets/Scripts/Item/Gun.cs
@@ -1,20 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private float damage;
+    [SerializeField] private float fireRate = 0.1f; // 1f = 1second
+    [SerializeField] private float amountOfAmmo = 5f;
+    [SerializeField] private Text textOfAmountOfAmmo;
 
+    private float _nextTimeToFire = 0f;
+    private float _amountOfAmmoSave;
+
+    private void Awake()
+    {
+        _amountOfAmmoSave = amountOfAmmo;
+    }
+    private void Update()
+    {
+        textOfAmountOfAmmo.text = $"{amountOfAmmo} / {_amountOfAmmoSave}";
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
     public void Shoot()
     {
-        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-        ray.origin = cam.transform.position; 
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if(Time.time >= _nextTimeToFire && amountOfAmmo != 0)
         {
-            hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(damage);
-            Debug.Log(hit.collider.name);
-        }
+            _nextTimeToFire = Time.time + fireRate;
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+            ray.origin = cam.transform.position;
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                hit.collider.gameObject
+                    .GetComponent<IDamageable>()?
+                    .TakeDamage(damage);
+            }
+            amountOfAmmo -= 1;
+        }     
+    }
+
+    private void Reload()
+    {
+        amountOfAmmo = _amountOfAmmoSave;
     }
 }
